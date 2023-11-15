257fab1642e402666afb993212dc1ecfd79a7791
Kilian Kaiping <852119+krnd@users.noreply.github.com>
2023-11-15 08:16:53 +0100

    feat(invoke-build): configure common tasks only if missing

--- invoke-build/common.build.ps1
+++ invoke-build/common.build.ps1
@@ -1,12 +1,41 @@
 #Requires -Version 5.1
 
 
+# ################################ FUNCTIONS ###################################
+
+function __InvokeBuild::IsTaskDefined {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Name
+    )
+    return ${*}.All.Contains($Name)
+}
+
+function __InvokeBuild::IsTaskMissing {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Name
+    )
+    return -not ${*}.All.Contains($Name)
+}
+
+
 # ################################ TASKS #######################################
 
 # ###################### Defaults ##########################
 
-TASK .
-TASK ..
+INVOKEBUILD:SETUP {
+    if (__InvokeBuild::IsTaskMissing ".") {
+        TASK .
+    }
+    if (__InvokeBuild::IsTaskMissing "..") {
+        TASK ..
+    }
+}
 
 
 # ###################### File Manager ######################
@@ -14,9 +43,9 @@ TASK ..
 TASK / {
     $Platform = $PSVersionTable.Platform
     if ($null -eq $Platform) {
-        explorer .
+        EXEC { explorer . }
     } elseif ($Platform -eq "Win32NT") {
-        explorer .
+        EXEC { explorer . }
     } else {
         Write-Warning "Task '/' not available for platform '$Platform'."
     }
