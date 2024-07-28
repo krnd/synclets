7b51eabbca7b870154e02ff6ac601af87bebbd8a
krnd <krnd@users.noreply.github.com>
2024-07-28 16:33:07 +0200

    feat(invoke-build): add zip extension for bundle plugin

---
+++ invoke-build/bundle.zip.extension.ps1
@@ -0,0 +1,23 @@
+#Requires -Version 5.1
+
+
+# ################################ FUNCTIONS ###################################
+
+__InvokeBuild::Plugin::Bundle::MakeBundler zip {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Artifact,
+        [Parameter(Mandatory)]
+        [string]
+        $Source,
+        [Parameter(ValueFromRemainingArguments)]
+        [string[]]
+        $Passthrough = @()
+    )
+    Compress-Archive $Source `
+        -DestinationPath $Artifact `
+        @Passthrough `
+        -Force
+}
