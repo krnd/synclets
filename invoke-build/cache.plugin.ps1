484a4dfb4013941b783a0ad0f3fed7e9e91c7032
krnd <krnd@users.noreply.github.com>
2024-08-02 17:28:54 +0200

    feat(invoke-build): add cache plugin

---
+++ invoke-build/cache.plugin.ps1
@@ -0,0 +1,60 @@
+#Requires -Version 5.1
+
+
+# ################################ VARIABLES ###################################
+
+$script:__InvokeBuild::Plugin::Cache = @{
+    File    = $MyInvocation.MyCommand.Name
+    Prefix  = "CACHE"
+    Storage = @{}
+}
+
+
+# ################################ FUNCTIONS ###################################
+
+function __InvokeBuild::Plugin::Cache::*STORE {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Name,
+        [Parameter(Mandatory, Position = 1)]
+        [AllowNull()]
+        [object]
+        $Value
+    )
+    $INVOKE = $script:__InvokeBuild
+    $PLUGIN = $INVOKE::Plugin::Cache
+    $STORAGE = $PLUGIN::Storage
+
+    $STORAGE[$Name] = $Value
+
+    return $Value
+}
+
+Set-Alias CACHE:STORE __InvokeBuild::Plugin::Cache::*STORE
+
+function __InvokeBuild::Plugin::Config::*GET {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Name,
+        [Parameter()]
+        [AllowNull()]
+        [object]
+        $Default
+    )
+    $INVOKE = $script:__InvokeBuild
+    $PLUGIN = $INVOKE::Plugin::Config
+    $STORAGE = $PLUGIN::Storage
+
+    if (-not $STORAGE.ContainsKey($Name)) {
+        return $Default
+    } else {
+        return $STORAGE[$Name]
+    }
+}
+
+Set-Alias CACHE:GET __InvokeBuild::Plugin::Config::*GET
+Set-Alias CACHE __InvokeBuild::Plugin::Config::*GET
