c3ff5bdec709b26896ef7e1eb83a496219ef9908
krnd <krnd@users.noreply.github.com>
2025-01-05 17:15:46 +0100

    feat(invoke-build): add code signing build script

---
+++ invoke-build/codesigning.build.ps1
@@ -0,0 +1,30 @@
+#Requires -Version 5.1
+
+
+# ################################ CONFIGURATION ###############################
+
+CONFIGURE codesigning.certificate `
+    -Default $null
+
+CONFIGURE codesigning.paths `
+    -Default @()
+
+
+# ################################ TASKS #######################################
+
+TASK codesigning:sign {
+    $Filter = $null
+    foreach ($Path in (CONF codesigning.paths)) {
+        (Get-ChildItem (Get-ChildItem $Path -Recurse) -File -Filter $Filter) `
+            | Add-CodeSigningCertificate -Like `
+                -Certificate (CONF codesigning.certificate)
+    }
+}
+
+TASK codesigning:remove {
+    $Filter = $null
+    foreach ($Path in (CONF codesigning.paths)) {
+        (Get-ChildItem (Get-ChildItem $Path -Recurse) -File -Filter $Filter) `
+            | Remove-CodeSigningCertificate
+    }
+}
