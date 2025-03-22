2cf6a76342dab9cd2d876c80d1160025f4960781
krnd <krnd@users.noreply.github.com>
2025-03-22 18:35:06 +0100

    feat(invoke-build): add python datamodel-code-generator build script

---
+++ invoke-build/python.schema.build.ps1
@@ -0,0 +1,43 @@
+# python.schema.build.ps1 1.0
+#Requires -Version 5.1
+
+
+# ################################ VARIABLES ###################################
+
+$script:__InvokeBuild::Builder["python.schema"] = @{}
+
+
+# ################################ CONFIGURATION ###############################
+
+CONFIGURE python.schema.models `
+    -Default $null
+
+CONFIGURE python.schema.basepath `
+    -Default ""
+
+
+# ################################ TASKS #######################################
+
+TASK python:ieb:schema python:venv:activate, {
+    $Models = (CONF python.schema.models)
+    $BasePath = (CONF python.schema.basepath)
+
+    $Models.PSObject.Properties | ForEach-Object {
+        $ModelFile, $SchemaFile = $_.Name, $_.Value
+
+        if ($BasePath) {
+            $ModelFile = (Join-Path $BasePath $ModelFile)
+            $SchemaFile = (Join-Path $BasePath $SchemaFile)
+        }
+
+        EXEC {
+            datamodel-codegen `
+                --use-schema-description `
+                --use-field-description `
+                --input $SchemaFile `
+                --input-file-type jsonschema `
+                --output $ModelFile `
+                --output-model-type typing.TypedDict
+        }
+    }
+}
